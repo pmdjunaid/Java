caf26200d112a1538cfc1eebc6642a34a0f92cdf
Junaid <justjunaid2002@gmail.com>
2024-10-02 23:36:54 +0530

    adding array folder

---
+++ Arrays/sum.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class sum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i : arr) {
+            sum+=i;
+        }
+
+        System.out.println("Sum of array elements is "+sum);
+    }
+}
